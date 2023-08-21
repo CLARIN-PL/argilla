6f4de00af804daacfb462e08c22b82397ef31c5c
artikandri <argikartika@gmail.com>
2023-08-21 20:58:02 +0200

    feat: hide

--- frontend/components/feedback-task/RecordFeedbackTask.component.vue
+++ frontend/components/feedback-task/RecordFeedbackTask.component.vue
@@ -6,7 +6,7 @@
       :key="id"
     >
       <TextFieldComponent
-        v-if="isTextType"
+        v-if="isTextType && !!content"
         :title="name"
         :fieldText="content"
         :useMarkdown="settings.use_markdown"
